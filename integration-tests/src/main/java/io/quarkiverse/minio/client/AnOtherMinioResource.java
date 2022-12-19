75ddeb196a4768ee92a51a6baa921f8e2f47d7f5
Stefan Gürtler <stefan.guertler@swarco.com>
2022-12-19 12:56:51 +0100

    Updated both minio and okhttp
    According to issue #12680 updating okhttp was blocked by updating Fabric8 Kubernetes Client to 6.x
    https://github.com/quarkusio/quarkus/issues/12680
    
    Since commit f1a50bf that precondition is now met!
    https://github.com/quarkusio/quarkus/commit/f1a50bfe683ea075f67e29321d84508433ed7c63

--- integration-tests/src/main/java/io/quarkiverse/minio/client/AnOtherMinioResource.java
+++ integration-tests/src/main/java/io/quarkiverse/minio/client/AnOtherMinioResource.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
 
-import javax.inject.Inject;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
 
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
