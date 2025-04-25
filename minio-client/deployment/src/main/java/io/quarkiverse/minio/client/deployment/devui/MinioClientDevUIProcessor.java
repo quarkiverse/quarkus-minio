4136ec082d8515142ea2413dc5c26e0bdcdb75da
Jérôme Tama <jerome.tama@gmail.com>
2025-04-25 16:21:55 +0200

    Removes startup warnings. (#425)
    See #422

--- minio-client/deployment/src/main/java/io/quarkiverse/minio/client/deployment/devui/MinioClientDevUIProcessor.java
+++ minio-client/deployment/src/main/java/io/quarkiverse/minio/client/deployment/devui/MinioClientDevUIProcessor.java
@@ -1,6 +1,6 @@
 package io.quarkiverse.minio.client.deployment.devui;
 
-import io.quarkiverse.minio.client.MiniosBuildTimeConfiguration;
+import io.quarkiverse.minio.client.deployment.devservices.DevServicesMinioProcessor;
 import io.quarkiverse.minio.client.deployment.devservices.MinioBuildTimeConfig;
 import io.quarkiverse.minio.client.devui.MinioJsonRPCService;
 import io.quarkus.deployment.IsDevelopment;
@@ -13,8 +13,9 @@ import io.quarkus.devui.spi.page.Page;
 public class MinioClientDevUIProcessor {
 
     @BuildStep(onlyIf = IsDevelopment.class)
-    void createCard(BuildProducer<CardPageBuildItem> cardProducer, MinioBuildTimeConfig devServiceConfig,
-            MiniosBuildTimeConfiguration configuration) {
+    void createCard(BuildProducer<CardPageBuildItem> cardProducer,
+            MinioBuildTimeConfig devServiceConfig,
+            DevServicesMinioProcessor.MinioConsoleURLBuildItem minioConsoleURLBuildItem) {
 
         final CardPageBuildItem card = new CardPageBuildItem();
 
@@ -22,7 +23,7 @@ public class MinioClientDevUIProcessor {
 
         card.addPage(Page.externalPageBuilder("Min.io console")
                 .icon("font-awesome-solid:signs-post")
-                .dynamicUrlJsonRPCMethodName("getMinioConsoleUrl")
+                .url(minioConsoleURLBuildItem.getUrl())
                 .doNotEmbed());
 
         card.setCustomCard("qwc-minio-card.js");
